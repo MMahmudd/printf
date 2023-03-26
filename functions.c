e6e5b4ef2be402770555403a614adf0d6fb4b07c
MMahmudd <122807656+MMahmudd@users.noreply.github.com>
2023-03-26 17:51:08 +0400

    update with mr christ

--- functions.c
+++ functions.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/************************* PRINT CHAR *************************/
+/************************* PRINT CHAR ************************/
 /**
  * print_char - Prints a char
  * @types: List a of arguments
